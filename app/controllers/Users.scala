cf27b4055cf41150bb6ecb143626d08237aebfa2
CelyaRousseau <celyarousseau.pro@gmail.com>
2016-02-07 16:40:22 +0100

    feat(create CRUD)

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,13 +1,61 @@
 package controllers
 
-object Users {
-  def getUsers() = play.mvc.Results.TODO
+import java.util.UUID
+import javax.inject.Inject
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-  def getUser(user_id: Long) = play.mvc.Results.TODO
+import models.User
+import play.api.libs.json.Json
+import play.api.mvc._
+import services.UserService
 
-  def createUser() = play.mvc.Results.TODO
+import scala.concurrent.Future
 
-  def updateUser(user_id: Long) = play.mvc.Results.TODO
+class Users @Inject()(userService: UserService) extends Controller {
+  def getUsers() = Action {
+    Ok(Json.toJson(userService.getAll()))
+  }
 
-  def deleteUser(user_id: Long) = play.mvc.Results.TODO
+  def getUser(user_id: String) = Action {
+
+    userService.get(UUID.fromString(user_id)) match {
+      case Some(user) => Ok(Json.toJson(user))
+      case None => NotFound
+    }
+  }
+
+  // TODO: create account
+  def createUser() = Action.async(parse.json) { request =>
+    val uuid: String = (request.body \ "uuid").as[String]
+    val role_id: Long = (request.body \ "role_id").as[Long]
+    val pseudo: String = (request.body \ "pseudo").as[String]
+    val user = new User(UUID.fromString(uuid), 0, role_id, pseudo)
+    val id:Future[Int] = userService.create(user)
+
+    id map {
+      case -1 => BadRequest("The user could not be created")
+      case _ => Created("The user has been successfully created")
+    }
+  }
+
+  def updateUser(user_id: String) = Action(parse.json) { implicit request =>
+    val account_id: Long = (request.body \ "account_id").as[Long]
+    val role_id: Long = (request.body \ "role_id").as[Long]
+    val pseudo: String = (request.body \ "pseudo").as[String]
+    val flag_connection: Option[Int] = (request.body \ "flag_connection").asOpt[Int]
+    val token: Option[String] = (request.body \ "token").asOpt[String]
+    val town_id: Option[Long] = (request.body \ "town_id").asOpt[Long]
+
+    val user = new User(UUID.fromString(user_id), account_id, role_id, pseudo, flag_connection, token, town_id)
+    userService.update(user) match {
+      case 0 => BadRequest("The user could not be updated")
+      case _ => {
+        Ok("The user has been successfully updated")
+      }
+    }
+  }
+
+  def deleteUser(user_id: String) = Action {
+    Ok(Json.toJson(userService.delete(UUID.fromString(user_id))))
+  }
 }
--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -1,17 +1,77 @@
 package services
 
-import models.User
-import play.api.Play._
-import play.api.db._
+import java.util.UUID
+
 import anorm._
+import com.google.inject.Inject
+import models.User
+import play.api.Play.current
+import play.api.db._
+import play.api.libs.json.Json
+import play.api.libs.ws._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-class UserService {
+class UserService @Inject()(ws: WSClient) {
 
-  def getAll() = {
+  def getAll(): List[User] = {
     val results: List[User] = DB.withConnection { implicit c =>
-      SQL( """SELECT * FROM USER """).as(User.parser.*)
+      SQL( """SELECT * FROM USER""").as(User.parser.*)
     }
     results
+  }
+
+  def get(user_id: UUID): Option[User] = {
+    val result: Option[User] = DB.withConnection { implicit c =>
+      SQL( """SELECT * FROM USER WHERE UUID = {user_id}""").on('user_id -> user_id).as(User.parser.singleOpt)
+    }
+    result
+  }
+
+  // TODO: create an account before create user
+  def create(user: User):Future[Int] = {
+    val data = Json.obj(
+      "account_balance" -> 200,
+      "description" -> " "
+    )
+    val url = "http://localhost:9000/accounts"
+    val futureResponse: Future[WSResponse] = ws.url(url).post(data)
+
+    val result: Future[Int] = futureResponse map { response: WSResponse =>
+      insert(user, response.body.toLong)
+    }
+    result
+  }
+
+  def insert(user:User, account_id:Long):Int = {
+    val id:Int = DB.withConnection { implicit c =>
+      SQL("INSERT INTO USER(UUID, ID_ACCOUNT, ID_ROLE, PSEUDO) VALUES({uuid}, {account_id}, {role_id}, {pseudo})")
+        .on('uuid -> user.uuid, 'account_id -> account_id, 'role_id -> user.role_id, 'pseudo -> user.pseudo).executeUpdate()
+    }
+    id
+  }
+
+  def update(user: User): Int = {
+    val id: Int = DB.withConnection { implicit c =>
+      SQL("UPDATE USER SET " +
+        "UUID = {uuid}, " +
+        "ID_ACCOUNT = {account_id}, " +
+        "ID_TOWN = {town_id}, " +
+        "ID_ROLE = {role_id}, " +
+        "PSEUDO = {pseudo}, " +
+        "TOKEN = {token}, " +
+        "FLAGCONNECTION = {flag_connection} WHERE UUID = {uuid}")
+        .on('uuid -> user.uuid, 'account_id -> user.account_id, 'town_id -> user.town_id, 'role_id -> user.role_id, 'pseudo -> user.pseudo, 'token -> user.token, 'flag_connection -> user.flag_connection).executeUpdate()
+    }
+    id
+  }
+
+  // TODO: delete account/badges associés (transaction maybe)
+  def delete(user_id: UUID): Int = {
+    val id: Int = DB.withConnection { implicit c =>
+      SQL("DELETE FROM USER WHERE UUID = {user_id}").on('user_id -> user_id).executeUpdate()
+    }
+    id
   }
 }
